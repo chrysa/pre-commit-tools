6922033724b0e835d87d2696a90deafb00d2acfb
chrysa <greau.anthony@gmail.com>
2023-09-20 21:34:07 +0000

    fixrewrite dockerfile formatter

--- pre_commit_hooks/python/format_dockerfile.py
+++ pre_commit_hooks/python/format_dockerfile.py
@@ -7,115 +7,99 @@ from dataclasses import dataclass
 from pathlib import Path
 
 from dockerfile_parse import DockerfileParser
-
-from pre_commit_hooks.python.tools.pre_commit_tools import PreCommitTools
+from tools.pre_commit_tools import PreCommitTools
 
 KEYWORDS_GROUP = ['ADD', 'ARG', 'COPY']
-KEYWORDS_SEPARATE = [
-    'ENTRYPOINT',
-    'HEALTHCHECK',
-    'ENV',
-    'FROM',
-    'RUN',
-    'SHELL',
-    'USER',
-]
 
 
+# TODO: separate block for litteral ARGS and ARGS composed with variable
 @dataclass
 class FormatDockerfile:
     dockerfile: Path = None
-    content: str = None
+    content: str = ''
     parser: DockerfileParser = DockerfileParser()
 
-    def _get_lines_startswith(self, *, keyword):
-        lines = []
-        for line in self.content.splitlines():
-            if line.startswith(keyword):
-                lines.append(line)
-        return lines
+    @staticmethod
+    def _get_instruction(*, line: dict[str, str | int]):
+        # print(f"get instuction type for {line}")
+        return line['instruction']
 
-    def format(self):
-        self.parser.content = self.content
-        self.content = self.parser.content
+    @staticmethod
+    def _get_line_content(*, line):
+        return line['content']
 
-    def format_grouped_keyword(self, *, keyword):
-        # print(f"fomat keyword {keyword} ..........")
-        line_tab = self.content.split('\n')
-        for index, line in enumerate(line_tab):
-            if line.startswith(keyword):
-                if not line_tab[index - 1].startswith(keyword) and not line_tab[index - 1].startswith(
-                    f'--replace--{keyword}',
-                ):
-                    line_tab[index] = f'--replace--{line}'
-                elif not line_tab[index + 1].startswith(keyword):
-                    line_tab[index] = f'{line}--replace--'
-        self.content = '\n'.join(line_tab)
+    @staticmethod
+    def _remove_split_lines(*, content):
+        # print("remove split lines ..........")
+        return re.sub(r' \\\n +', ' ', content)
 
-    def remove_multiple_space(self):
-        # print("remove multiple space ..........")
-        self.content = re.sub(r' +', ' ', self.content)
+    def _is_grouped_keyword(self, *, line: dict[str, int | str]) -> bool:
+        # print(f"test {line['instruction']} is a grouped keyword ..........")
+        return self._get_instruction(line=line) in KEYWORDS_GROUP
 
-    def format_multilines_env(self):
-        # print("format multi line env ..........")
-        for line in self._get_lines_startswith(keyword='ENV'):
-            multiline = ' \\\n  '.join(line.split(' ')[1:])
-            self.content = self.content.replace(line, f'ENV {multiline}')
+    def _is_same_as_previous(self, *, index: int) -> bool:
+        # print(f"test if line {index} type {self.parser.structure[index-1]['instruction']} is same as previous {self.parser.structure[index]['instruction']} ..........")
+        if index == 0:
+            state = False
+        else:
+            state = self._get_instruction(line=self.parser.structure[index - 1]) == self._get_instruction(
+                line=self.parser.structure[index],
+            )
+        return state
 
-    def format_multilines_run(self):
-        # print("format multi line cmd ..........")
-        for line in self._get_lines_startswith(keyword='RUN'):
-            multiline = ' \\\n  && '.join(line.split('&&'))
-            self.content = self.content.replace(line, multiline)
+    def _is_type(self, *, line: dict[str, int | str], instruction_type: str) -> bool:
+        # print(f"check if line {line} is {instruction_type} ..........")
+        return self._get_instruction(line=line) == instruction_type
 
-    def format_split_healthcheck(self):
-        for line in self._get_lines_startswith(keyword='HEALTHCHECK'):
-            multiline = '\\\n   CMD '.join(line.split('CMD'))
-            self.content = self.content.replace(line, multiline)
-
-    def read(self, *, dockerfile: Path):
-        # print(f"read {dockerfile} ..........")
-        self.dockerfile = dockerfile
-        with open(self.dockerfile) as stream:
-            self.content = stream.read()
-
-    def remove_multiple_empty_lines(self):
-        self.content = re.sub(r'\n+', '\n', self.content)
-
-    def remove_split_lines(self):
-        # print(f"remove split lines ..........")
-        self.content = re.sub(r' \\\n +', ' ', self.content)
-
-    def save(self):
-        # print(f"update {self.dockerfile} ..........")
-        with open(self.dockerfile, 'w+') as stream:
-            stream.seek(0)
-            stream.write(self.content)
-            stream.truncate()
-
-    def format_separated_keyword(self, *, keyword):
-        # print(f"fomat keyword {keyword} ..........")
-        line_tab = self.content.split('\n')
-        for index, line in enumerate(line_tab):
-            if line.startswith(keyword):
-                line_tab[index] = f'\n{line}\n'
-        self.content = '\n'.join(line_tab)
+    def _load_dockerfile(self, *, dockerfile_path: Path) -> None:
+        print(f'read {file} ..........')
+        with open(dockerfile_path) as stream:
+            self.parser.content = self._remove_split_lines(content=stream.read())
 
     def format_file(self, *, file):
-        self.read(dockerfile=file)
-        self.format()
-        self.remove_multiple_empty_lines()
-        self.remove_split_lines()
-        self.remove_multiple_space()
-        for keyword in KEYWORDS_GROUP:
-            self.format_grouped_keyword(keyword=keyword)
-        self.content = self.content.replace('--replace--', '\n')
-        for keyword in KEYWORDS_SEPARATE:
-            self.format_separated_keyword(keyword=keyword)
-        self.format_multilines_run()
-        self.format_multilines_env()
-        self.format_split_healthcheck()
-        self.save()
+        # format
+        for index, line in enumerate(self.parser.structure):
+            print(line)
+            if self._is_type(line=line, instruction_type='COMMENT'):
+                self.content += self._get_line_content(line=line)
+            elif self._is_type(line=line, instruction_type='FROM'):
+                self.content += '\n'
+                self.content += '\n'
+                self.content += self._get_line_content(line=line)
+            elif self._is_type(line=line, instruction_type='ENV'):
+                # print("format multi line env ..........")
+                multiline = ' \\\n  '.join(self._get_line_content(line=line).split(' ')[1:])
+                self.content += '\n'
+                self.content += f'ENV {multiline}'
+            elif self._is_type(line=line, instruction_type='RUN'):
+                multiline = ' \\\n  && '.join(self._get_line_content(line=line).split('&&'))
+                self.content += '\n'
+                self.content += multiline
+            elif self._is_type(line=line, instruction_type='HEALTHCHECK'):
+                # print("format healthcheck ..........")
+                multiline = '\\\n   CMD '.join(self._get_line_content(line=line).split('CMD'))
+                self.content += '\n'
+                self.content += multiline
+            elif self._is_grouped_keyword(line=line):
+                if self._is_same_as_previous(index=index):
+                    self.content += self._get_line_content(line=line)
+                else:
+                    self.content += '\n'
+                    self.content += self._get_line_content(line=line)
+            else:
+                self.content += '\n'
+                self.content += self._get_line_content(line=line)
+
+        if self.content != self.parser.content:
+            # print(f"update {self.dockerfile} ..........")
+            with open(file, 'w+') as stream:
+                stream.seek(0)
+                stream.write(self.content)
+                stream.truncate()
+            status = 'formatted'
+        else:
+            status = 'unchanged'
+        print(f'save {file} .......... {status}')
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -123,8 +107,9 @@ def main(argv: Sequence[str] | None = None) -> int:
     tools_instance = PreCommitTools()
     args = tools_instance.set_params(help_msg='format dockerfile', argv=argv)
     for file in args.filenames:
+        format_dockerfile_class.load_dockerfile(dockerfile_path=Path(file))
         format_dockerfile_class.format_file(file=Path().absolute() / file)
-        print(f'save {file} .......... done')
+        format_dockerfile_class.save()
 
 
 if __name__ == '__main__':

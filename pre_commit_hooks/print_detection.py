8097bc80f8d7d9c1d467acf6976fab4347ea4514
chrysa <chrysa@users.noreply.github.com>
2023-09-25 12:45:53 +0000

    add logging policy

--- pre_commit_hooks/print_detection.py
+++ pre_commit_hooks/print_detection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from __future__ import annotations
 
+import logging
 import re
 from pathlib import Path
 
@@ -10,26 +11,38 @@ PRINT_RE = re.compile(r'\bprint\(')
 COMMENTED_RE = re.compile(r'\s#\sprint\(')
 DISABLE_COMMENT_RE = re.compile(r'\bprint-detection\s*:\s*disable')
 
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
 
 def as_print(*, line):
+    logger.debug(f'{line} | presence -> {bool(PRINT_RE.search(line))}')
     return bool(PRINT_RE.search(line))
 
 
 def is_commented(*, line):
+    logger.debug(f'{line} | commented -> {bool(COMMENTED_RE.search(line))}')
     return bool(COMMENTED_RE.search(line))
 
 
 def is_disabked(*, line):
+    logger.debug(f'{line} | disabled -> {bool(DISABLE_COMMENT_RE.search(line))}')
     return bool(DISABLE_COMMENT_RE.search(line))
 
 
-def main(argve) -> int:
+def main(argv: Sequence[str] | None = None) -> int:
     tools_instance = PreCommitTools()
     args = tools_instance.set_params(help_msg='search print on python code', argv=argv)
     ret_val = 0
     for file in args.filenames:
         file = Path(file)
         with open(file) as stream:
+            logging.debug(f'process file {file}')
             for line_number, line_content in enumerate(stream.readlines()):
                 if (
                     as_print(line=line_content)
@@ -37,7 +50,7 @@ def main(argve) -> int:
                     and not is_commented(line=line_content)
                 ):
                     print(f'[{file}][L.{line_number}] {line_content}')  # print-detection: disable
-                ret_val = 1
+                    ret_val = 1
     return ret_val
 
 

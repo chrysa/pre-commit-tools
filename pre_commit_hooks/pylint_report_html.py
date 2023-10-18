898901ad942bc28cd5500e6524c405727d095780
chrysa <chrysa@users.noreply.github.com>
2023-10-18 13:27:50 +0000

    investigate pylint report generation crash

--- pre_commit_hooks/pylint_report_html.py
+++ pre_commit_hooks/pylint_report_html.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 from pylint.lint import Run
+from pylint_report.pylint_report import CustomJsonReporter
 from pylint_report.pylint_report import main as report_main
 
 from pre_commit_hooks.tools.pre_commit_tools import PreCommitTools
@@ -29,32 +30,21 @@ class PylintHtmlReport(PreCommitTools):
     namesapace_args: argparse.Namespace
     pylint_args: list[str]
 
-    # def clean_json_report(self):
-    #     if self.namesapace_args.output_json is None:
-    #         logger.debug("clean json report")
-    #         self.output_json.unlink()
+    def clean_json_report(self):
+        if self.namesapace_args.output_json is None:
+            logger.debug("clean json report")
+            self.output_json.unlink()
 
-    # def convert_json_to_html(self):
-    #     logger.debug("convert json to html")
-    #     report_main([self.output_json.as_posix(), "-o", self.output_html.as_posix()])
+    def convert_json_to_html(self):
+        logger.debug("convert json to html")
+        print([self.output_json.as_posix(), "-o", self.output_html.as_posix()])
+        report_main(argv=[self.output_json.as_posix(), "-o", self.output_html.as_posix()])
 
-    def get_args(self, argv):
-        logger.debug("parse arguments")
-        self.namesapace_args, self.pylint_args = super().get_args(argv=argv)
+    def create_parent_report(self, *, report_path: str) -> None:
+        logger.debug(f"create {report_path.parent}")
+        report_path.parent.mkdir(parents=True, exist_ok=True)
 
-    def generate_json_report(self):
-        logger.debug(f"generate json report on {self.output_json}")
-        try:
-            Run(
-                self.pylint_args
-                + ["--output", self.output_json.as_posix()]
-                + ["--load-plugins=pylint_report", "--output-format=pylint_report.CustomJsonReporter"]
-                + self.namesapace_args.filenames,
-            )
-        except SystemExit:
-            pass
-
-    def define_output_path(self, *, output_variable_name):
+    def define_output_path(self, *, output_variable_name: str):
         logger.debug(f"define {output_variable_name}")
         value = self.namesapace_args.__dict__[output_variable_name]
         if isinstance(value, str):
@@ -63,7 +53,20 @@ class PylintHtmlReport(PreCommitTools):
             output = Path().cwd() / value
         else:
             output = value
+        self.create_parent_report(report_path=output.parent)
         return output
+
+    def generate_ouptput_path(self):
+        self.output_html = self.define_output_path(output_variable_name="output_html")
+        self.output_json = self.define_output_path(output_variable_name="output_json")
+
+    def get_args(self):
+        logger.debug("parse arguments")
+        self.namesapace_args, self.pylint_args = super().get_args()
+
+    def run_pylint(self):
+        logger.debug("run pylint")
+        Run(self.pylint_args, do_exit=False)
 
     def set_params(self):
         logger.debug("define parser")
@@ -81,22 +84,26 @@ class PylintHtmlReport(PreCommitTools):
             default=Path().cwd() / "pylint_report.json",
         )
 
+    def update_pylint_args(self):
+        logger.debug("update pylint args")
+        # self.pylint_args.extend(["--exit-zero", "--persistent=n", "--reports=n", "--score=n"])
+        # self.pylint_args.extend(
+        #     [f"--output={self.output_json.as_posix()}", "--output-format=pylint_report.CustomJsonReporter"],
+        # )
+        self.pylint_args.extend(self.namesapace_args.filenames)
+        print(self.pylint_args)
+
 
 def main(argv: Sequence[str] | None = None) -> int:
     instance = PylintHtmlReport()
     instance.set_params()
-    instance.get_args(argv=argv)
-    instance.output_html = instance.define_output_path(output_variable_name="output_html")
-    instance.output_json = instance.define_output_path(output_variable_name="output_json")
-    instance.output_html.parent.mkdir(parents=True, exist_ok=True)
-    instance.output_json.parent.mkdir(parents=True, exist_ok=True)
-    instance.generate_json_report()
-    # instance.convert_json_to_html()
-    # instance.clean_json_report()
-    # if instance.file_exist(file=instance.output_html) and not instance.file_empty(file=instance.output_html):
-    #     return 0
-    # else:
-    #     return 1
+    instance.get_args()
+    instance.generate_ouptput_path()
+    instance.update_pylint_args()
+    instance.run_pylint()
+    instance.convert_json_to_html()
+    instance.clean_json_report()
+    return instance.file_exist(file=instance.output_html) and not instance.file_empty(file=instance.output_html)
 
 
 if __name__ == '__main__':

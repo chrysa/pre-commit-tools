2e7c9d450f25ed3fb403ed7be2a725ad679e388f
chrysa <chrysa@users.noreply.github.com>
2025-03-28 18:17:44 +0100

    Update format_dockerfile.py

--- pre_commit_hooks/format_dockerfile.py
+++ pre_commit_hooks/format_dockerfile.py
@@ -174,7 +174,7 @@ class FormatDockerfile:
             logger.debug(f'update {file} ..........')
             with open(file, 'w+') as stream:
                 stream.seek(0)
-                stream.write(self.content)
+                stream.write(self.content.strip())
                 stream.truncate()
             print(f'{file} .......... formatted')
             self.return_value = 1
